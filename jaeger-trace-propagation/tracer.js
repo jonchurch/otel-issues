2bcd21a65d0ea0d1a41b64a31ecf5c7b64c9e775
jonchurch <me@jonchurch.com>
2021-07-08 14:41:55 -0400

    initial commit of remote service trace prop

---
+++ jaeger-trace-propagation/tracer.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const opentelemetry = require('@opentelemetry/api');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { NodeTracerProvider } = require('@opentelemetry/node');
+const { Resource } = require('@opentelemetry/resources');
+const { ResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+
+const EXPORTER = process.env.EXPORTER || '';
+
+module.exports = (serviceName) => {
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [ResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  let exporter;
+  if (EXPORTER.toLowerCase().startsWith('z')) {
+    exporter = new ZipkinExporter();
+  } else {
+    exporter = new JaegerExporter();
+  }
+
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+  // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
+  provider.register();
+
+  registerInstrumentations({
+    // // when boostraping with lerna for testing purposes
+    instrumentations: [
+      new HttpInstrumentation(),
+    ],
+  });
+
+  return opentelemetry.trace.getTracer('http-example');
+};
